60b6bd0c5d62d5b7d174387d0eec5254a46dfb78
Aspen <zebington@gmail.com>
2018-12-04 10:26:54 +0000

    part two

--- src/solutions/four.py
+++ src/solutions/four.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 __author__ = "Aspen Thompson"
 
 
-def part_one(lines):
+def generate_guards(lines):
     lines.sort()
     guards = defaultdict(Guard)
     current_id = 0
@@ -11,19 +11,39 @@ def part_one(lines):
         parts = line.split()
         if parts[2] == "Guard":
             current_id = parts[3][1:]
+            guards[current_id].id = current_id
         else:
             guards[current_id].add_log(line)
+    return guards
 
-    sleepy_id = max(guards, key=lambda guard: guards[guard].sleep_duration)
-    return max(guards[sleepy_id].sleep_minutes, key=guards[sleepy_id].sleep_minutes.get) * int(sleepy_id)
+
+def part_one(lines):
+    guards = generate_guards(lines)
+    sleepy = guards[max(guards, key=lambda guard: guards[guard].sleep_duration)]
+    return max(sleepy.sleep_minutes, key=sleepy.sleep_minutes.get) * sleepy.id
+
+
+def part_two(lines):
+    guards = generate_guards(lines)
+    sleepy = guards[max(guards, key=lambda guard: guards[guard].sleepiest_minute[1])]
+    return sleepy.id * int(sleepy.sleepiest_minute[0])
 
 
 class Guard:
     def __init__(self):
+        self.id = 0
         self.log_lines = []
 
     def add_log(self, line):
         self.log_lines.append(line)
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, val):
+        self.__id = int(val)
 
     @property
     def sleep_times(self):
@@ -45,6 +65,13 @@ class Guard:
             for i in range(sleep_time[0], sleep_time[1]):
                 minutes[i] += 1
         return minutes
+
+    @property
+    def sleepiest_minute(self):
+        if len(self.sleep_minutes) == 0:
+            return 0, 0
+        minute = max(self.sleep_minutes, key=self.sleep_minutes.get)
+        return minute, self.sleep_minutes[minute]
 
     @property
     def sleep_duration(self):
